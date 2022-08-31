b2d8d2ee42664a2f1f6b9d84264b9ca00e281adb
Dillon <dillon.embreus@gmail.com>
2022-08-30 23:35:09 -0600

    Updated carousel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public slides: ISlide[];
+  constructor() {
+    this.slides = [
+      {
+        id: 0,
+        imageUrl:
+          'https://images.pexels.com/photos/2407634/pexels-photo-2407634.jpeg?cs=srgb&dl=pexels-jeff-guab-2407634.jpg&fm=jpg',
+      },
+      {
+        id: 1,
+        imageUrl:
+          'https://images.pexels.com/photos/1701893/pexels-photo-1701893.jpeg?cs=srgb&dl=pexels-ricardo-esquivel-1701893.jpg&fm=jpg',
+      },
+      {
+        id: 2,
+        imageUrl:
+          'https://images.pexels.com/photos/7683376/pexels-photo-7683376.jpeg?cs=srgb&dl=pexels-mikhail-nilov-7683376.jpg&fm=jpg',
+      },
+    ];
   }
 
+  ngOnInit(): void {}
+}
+
+interface ISlide {
+  id: number;
+  imageUrl: string;
 }
